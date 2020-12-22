6088db0309d7fc7aa0fcc0cf81db504412dfe637
Alex <altipping@hotmail.co.uk>
2020-12-22 12:41:04 +0000

    footer + header + nav components added

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      {/* Copyright */}
+      <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+        Author: Alex Tipping
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navigation from "./Navigation.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header() {
+  return <Navigation />;
+}
+
+export default Header;
